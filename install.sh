0150c14f770250bb8ae8bc9b5159eaae8b6fa8dd
Kiltzx <dudu.cedsv@gmail.com>
2022-12-23 10:43:47 -0300

    update sources and added new software

--- install.sh
+++ install.sh
@@ -1,23 +1,24 @@
-sudo -u
+sudo -i
+sudo apt-get install snapd -y
 export PATH=$PATH:/snap/bin
 sudo apt-get install curl -y
-# Install chrome,code,discord,slack and dbeaver
+# Install some tools
 
-sudo apt-get install snapd -y
 sudo snap install slack --classic
 sudo snap install postman
 sudo snap install htop
 sudo snap install telegram-desktop
 sudo snap install discord
+sudo snap install enpass
 
 # Download .deb package
 mkdir install_dpkg
 cd install_dpkg
 
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-wget https://az764295.vo.msecnd.net/stable/899d46d82c4c95423fb7e10e68eba52050e30ba3/code_1.63.2-1639562499_amd64.deb
-wget https://download.dbeaver.com/community/21.3.2/dbeaver-ce_21.3.2_amd64.deb
-wget https://autoupdate.termius.com/linux/Termius.deb
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb #google-chrome
+wget https://az764295.vo.msecnd.net/stable/e8a3071ea4344d9d48ef8a4df2c097372b0c5161/code_1.74.2-1671533413_amd64.deb #vscode
+wget https://download.dbeaver.com/community/21.3.2/dbeaver-ce_21.3.2_amd64.deb #dbeaver
+wget https://autoupdate.termius.com/linux/Termius.deb #termius
 
 sudo dpkg -i *.deb
 
@@ -29,8 +30,17 @@ sudo apt-get update -y && sudo apt-get install spotify-client -y
 
 # Git configure
 # sudo apt-get install git -y # if you dont have git, remove this comment
-git config --global user.name "Kiltzx"
-git config --global user.email dudu.cedsv@gmail.com
+git config --global user.name  {{YOUR_USERNAME}}
+git config --global user.email {{YOUR_EMAIL}}
 
 # Gnome-Tweaks
 sudo apt install gnome-tweaks -y
+
+# vim
+sudo apt install vim -y
+
+# zsh
+sudo apt install zsh -y
+
+# autoremove
+sudo apt autoremove -y
